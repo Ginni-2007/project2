412b3505f23a0fa86722c09ac42cf2389fdf542c
mp2529 <m43681752@gmail.com>
2026-03-21 14:17:40 -0400

    # hello

---
+++ entities.py
@@ -0,0 +1,29 @@
+# changed this now
+# hello
+# hiuwhttuh4
+# rhfiwaheglgrehiwh
+# ifwoghaoiwfoaeihgoisehrgfoaifoeajrgoifjoihgseoij
+class Graph:
+    """
+
+    """
+
+
+
+
+class Race:
+    """
+    A single Race
+    """
+
+class Driver:
+    """
+    A single racer
+    """
+
+
+
+class RaceData:
+    """
+    contains a SINGLE racer's data for a single race based on raceID
+    """
